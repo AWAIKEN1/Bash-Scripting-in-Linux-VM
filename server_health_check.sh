2416bac5b87d7c48a0be2788d40a9dc60db81c19
Alistair <alistairaiken@gmail.com>
2024-01-19 16:18:25 +0000

    First part of script completed- CPU check

--- server_health_check.sh
+++ server_health_check.sh
@@ -1,2 +1,16 @@
-echo "Hello, this is my Bash Script"
-echo "Trying to get this to work"
+#!/bin/bash
+
+#Description: This script will perform a detailed health check of a Linux server
+
+
+check_cpu() {
+	local load=$(uptime | awk -F'load average: ' '{print $2}')
+	IFS=',' read -r load1 load5 load15 <<< "$load"
+
+	echo "1-minute Load Average: $load1"
+	echo "5-minute Load Average: $load5"
+	echo "15-minute Load Average: $load15"
+}
+	
+check_cpu
+	
