b4f78f00e29cc7d3d031c532d9ddb8ef3e19e99b
Alistair <alistairaiken@gmail.com>
2024-01-19 14:09:34 +0000

    A basic interactive script

--- interactiveshell.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Asking input from user for first name and then last name 
-echo What is your first name?
-read FIRST_NAME
-echo What is your last name?
-read LAST_NAME
-
-#Output the users first and last name 
-echo Hello $FIRST_NAME $LAST_NAME
