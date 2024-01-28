89eb7c1893176d258d67a7eaedf987b0a8c3cdc1
Alistair <alistairaiken@gmail.com>
2024-01-28 18:55:48 +0000

    Adding comments to functions to describe what each one is doing

--- server_health_check.sh
+++ server_health_check.sh
@@ -2,7 +2,7 @@
 
 #Description: This script will perform a detailed health check of a Linux server
 
-
+#Function to check CPU load
 check_cpu() {
 	local load=$(uptime | awk -F'load average: ' '{print $2}')
 	IFS=',' read -r load1 load5 load15 <<< "$load"
@@ -12,6 +12,7 @@ check_cpu() {
 	echo "15-minute Load Average: $load15"
 }
 
+#Function to check memory usage
 check_memory() {
 	echo "Memory Usage:"
     free -h | awk 'NR==2{printf "Total: %s\tUsed: %s\tFree: %s\n", $2, $3, $4}'
@@ -19,6 +20,7 @@ check_memory() {
     free -h | awk '/Swap/{printf "Total: %s\tUsed: %s\tFree: %s\n", $2, $3, $4}'
 }
 
+#Function to display system uptime 
 show_uptime() {
 	up=$(uptime -p | cut -c4- )
 	since=$(uptime -s)
@@ -30,6 +32,7 @@ It has been running since ${since}
 EOF
 }
 
+#Function to check network status
 check_network() {
     echo "Network Status:"
     for interface in $(ls /sys/class/net/ | grep -v lo); do
